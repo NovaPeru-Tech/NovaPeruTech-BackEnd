85f6a56e09b9b1506dd668dde1c313a0f9d4f5be
RenatoCY <renato.calvo2@hotmail.com>
2025-10-30 18:13:41 -0500

    feat(NursingHome): add resources for nursing homes

---
+++ src/main/java/com/novaperutech/veyra/platform/nursingHomes/interfaces/rest/resources/CreateNursingHomeResource.java
@@ -0,0 +1,5 @@
+package com.novaperutech.veyra.platform.nursingHomes.interfaces.rest.resources;
+
+public record CreateNursingHomeResource(String name,String ruc,String phoneNumber,String address, String description) {
+
+}
