0a5222a36a0faf5efafe20b75df77c9ab6b8b8c6
RenatoCY <renato.calvo2@hotmail.com>
2025-11-05 19:17:38 -0500

    feat(Profiles): add command and query services for BusinessProfile and PersonProfile

---
+++ src/main/java/com/novaperutech/veyra/platform/profiles/domain/services/BusinessProfileQueryService.java
@@ -0,0 +1,13 @@
+package com.novaperutech.veyra.platform.profiles.domain.services;
+
+import com.novaperutech.veyra.platform.profiles.domain.model.aggregates.BusinessProfile;
+import com.novaperutech.veyra.platform.profiles.domain.model.queries.GetAllBusinessProfileQuery;
+import com.novaperutech.veyra.platform.profiles.domain.model.queries.GetBusinessProfileByIdQuery;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface BusinessProfileQueryService {
+    Optional<BusinessProfile>handle(GetBusinessProfileByIdQuery query);
+    List<BusinessProfile> handle (GetAllBusinessProfileQuery query);
+}
