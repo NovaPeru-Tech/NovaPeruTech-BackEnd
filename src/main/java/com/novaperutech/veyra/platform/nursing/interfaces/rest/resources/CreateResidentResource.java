671303c6dab881b251d4ca130182e69f3bd2fd4d
RenatoCY <renato.calvo2@hotmail.com>
2025-11-07 09:36:22 -0500

    feat: add resources

---
+++ src/main/java/com/novaperutech/veyra/platform/nursing/interfaces/rest/resources/CreateResidentResource.java
@@ -0,0 +1,14 @@
+package com.novaperutech.veyra.platform.nursing.interfaces.rest.resources;
+
+import java.time.LocalDate;
+
+public record CreateResidentResource(String dni, String firstName, String lastName,
+                                     LocalDate birthDate, Integer Age, String emailAddress, String street,
+                                     String number,
+                                     String city,
+                                     String postalCode,
+                                     String country,
+                                     String photo, String phoneNumber, String legalRepresentativeFirstName
+        , String legalRepresentativeLastName, String legalRepresentativePhoneNumber
+        , String emergencyContactFirstName, String emergencyContactLastName, String emergencyContactPhoneNumber) {
+}
