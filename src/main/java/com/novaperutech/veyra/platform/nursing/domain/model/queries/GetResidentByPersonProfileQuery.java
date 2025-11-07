c507ad45ed1b4f4b22412d0ee4f218d2c685edde
RenatoCY <renato.calvo2@hotmail.com>
2025-11-07 09:36:32 -0500

    feat: add query

---
+++ src/main/java/com/novaperutech/veyra/platform/nursing/domain/model/queries/GetResidentByPersonProfileQuery.java
@@ -0,0 +1,6 @@
+package com.novaperutech.veyra.platform.nursing.domain.model.queries;
+
+import com.novaperutech.veyra.platform.nursing.domain.model.valueobjetcs.PersonProfileId;
+
+public record GetResidentByPersonProfileQuery(PersonProfileId id) {
+}
