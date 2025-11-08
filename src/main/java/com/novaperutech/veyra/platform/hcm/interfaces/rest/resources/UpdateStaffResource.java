71543589e7e1b1a823cf059ef0033c5b08ded8e0
RenatoCY <renato.calvo2@hotmail.com>
2025-11-07 22:14:40 -0500

    feat: add update staff command and resource

---
+++ src/main/java/com/novaperutech/veyra/platform/hcm/interfaces/rest/resources/UpdateStaffResource.java
@@ -0,0 +1,15 @@
+package com.novaperutech.veyra.platform.hcm.interfaces.rest.resources;
+
+import java.time.LocalDate;
+
+public record UpdateStaffResource(String dni, String firstName, String lastName,
+                                  LocalDate birthDate, Integer Age, String emailAddress, String street,
+                                  String number,
+                                  String city,
+                                  String postalCode,
+                                  String country,
+                                  String photo, String phoneNumber
+        , String emergencyContactFirstName,
+                                  String emergencyContactLastName,
+                                  String emergencyContactPhoneNumber) {
+}
