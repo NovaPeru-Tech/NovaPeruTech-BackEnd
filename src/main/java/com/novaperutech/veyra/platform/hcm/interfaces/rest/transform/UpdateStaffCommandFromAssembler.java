fce33366b8b9490849a6912e7be54d3ac4aba50c
RenatoCY <renato.calvo2@hotmail.com>
2025-11-07 22:01:58 -0500

    feat: add assemblers for staff

---
+++ src/main/java/com/novaperutech/veyra/platform/hcm/interfaces/rest/transform/UpdateStaffCommandFromAssembler.java
@@ -0,0 +1,13 @@
+package com.novaperutech.veyra.platform.hcm.interfaces.rest.transform;
+
+import com.novaperutech.veyra.platform.hcm.domain.model.commands.UpdateStaffCommand;
+import com.novaperutech.veyra.platform.hcm.interfaces.rest.resources.UpdateStaffResource;
+
+public class UpdateStaffCommandFromAssembler {
+    public static UpdateStaffCommand toCommandFromResource(Long id, UpdateStaffResource resource){
+        return new UpdateStaffCommand(id,resource.dni(),resource.firstName()
+                ,resource.lastName(),resource.birthDate(),resource.Age()
+                ,resource.emailAddress(),resource.street(),resource.number(),resource.city()
+                ,resource.postalCode(),resource.country(),resource.photo(),resource.phoneNumber(),resource.emergencyContactFirstName(),resource.emergencyContactLastName(),resource.phoneNumber());
+    }
+}
