f662d5952b77feb2e3e5d5ff05c33e2989b59567
RenatoCY <renato.calvo2@hotmail.com>
2025-11-10 13:44:44 -0500

    feat: implement queries for nursing home

---
+++ src/main/java/com/novaperutech/veyra/platform/nursing/application/internal/queryservices/NursingHomeQueryServiceImpl.java
@@ -0,0 +1,31 @@
+package com.novaperutech.veyra.platform.nursing.application.internal.queryservices;
+
+import com.novaperutech.veyra.platform.nursing.domain.model.aggregates.NursingHome;
+import com.novaperutech.veyra.platform.nursing.domain.model.queries.GetAllNursingHomeQuery;
+import com.novaperutech.veyra.platform.nursing.domain.model.queries.GetNursingHomeByBusinessProfileIdQuery;
+import com.novaperutech.veyra.platform.nursing.domain.services.NursingHomeQueryServices;
+import com.novaperutech.veyra.platform.nursing.infrastructure.persistence.jpa.repositories.NursingHomeRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class NursingHomeQueryServiceImpl implements NursingHomeQueryServices {
+private final NursingHomeRepository nursingHomeRepository;
+
+    public NursingHomeQueryServiceImpl(NursingHomeRepository nursingHomeRepository) {
+        this.nursingHomeRepository = nursingHomeRepository;
+
+    }
+
+    @Override
+    public Optional<NursingHome> handle(GetNursingHomeByBusinessProfileIdQuery query) {
+        return nursingHomeRepository.findByBusinessProfileId(query.businessProfileId());
+    }
+
+    @Override
+    public List<NursingHome> handle(GetAllNursingHomeQuery query) {
+        return nursingHomeRepository.findAll();
+    }
+}
