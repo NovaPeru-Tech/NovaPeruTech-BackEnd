dfd8745189d36db93c521d5c3da157b60b88c622
RenatoCY <renato.calvo2@hotmail.com>
2025-11-10 13:45:23 -0500

    feat: implement queries

---
+++ src/main/java/com/novaperutech/veyra/platform/nursing/domain/model/queries/GetNursingHomeByBusinessProfileIdQuery.java
@@ -0,0 +1,6 @@
+package com.novaperutech.veyra.platform.nursing.domain.model.queries;
+
+import com.novaperutech.veyra.platform.nursing.domain.model.valueobjetcs.BusinessProfileId;
+
+public record GetNursingHomeByBusinessProfileIdQuery(BusinessProfileId businessProfileId) {
+}
