7addf0e4126cddad8e66c26035406e417d730570
RenatoCY <renato.calvo2@hotmail.com>
2025-11-10 13:38:19 -0500

    feat: implement aggregate nursingHome

---
+++ src/main/java/com/novaperutech/veyra/platform/nursing/domain/model/aggregates/NursingHome.java
@@ -0,0 +1,18 @@
+package com.novaperutech.veyra.platform.nursing.domain.model.aggregates;
+
+import com.novaperutech.veyra.platform.nursing.domain.model.valueobjetcs.BusinessProfileId;
+import com.novaperutech.veyra.platform.shared.domain.model.aggregates.AuditableAbstractAggregateRoot;
+import jakarta.persistence.Embedded;
+import jakarta.persistence.Entity;
+import lombok.Getter;
+
+@Entity
+@Getter
+public class NursingHome extends AuditableAbstractAggregateRoot<NursingHome> {
+    @Embedded
+    private BusinessProfileId businessProfileId;
+    public NursingHome(){}
+    public NursingHome(BusinessProfileId businessProfileId) {
+        this.businessProfileId = businessProfileId;
+    }
+}
