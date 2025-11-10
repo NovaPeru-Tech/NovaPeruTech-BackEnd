ee9b9080c9631b204093466964bd5966da253753
RenatoCY <renato.calvo2@hotmail.com>
2025-11-09 19:55:08 -0500

    feat: implement commands and queries for contract

--- src/main/java/com/novaperutech/veyra/platform/hcm/domain/services/StaffCommandServices.java
+++ src/main/java/com/novaperutech/veyra/platform/hcm/domain/services/StaffCommandServices.java
@@ -1,9 +1,7 @@
 package com.novaperutech.veyra.platform.hcm.domain.services;
 
 import com.novaperutech.veyra.platform.hcm.domain.model.aggregates.Staff;
-import com.novaperutech.veyra.platform.hcm.domain.model.commands.CreateStaffCommand;
-import com.novaperutech.veyra.platform.hcm.domain.model.commands.DeleteStaffCommand;
-import com.novaperutech.veyra.platform.hcm.domain.model.commands.UpdateStaffCommand;
+import com.novaperutech.veyra.platform.hcm.domain.model.commands.*;
 
 import java.util.Optional;
 
@@ -11,4 +9,6 @@ public interface StaffCommandServices {
     Long handle(CreateStaffCommand command);
     Optional<Staff>handle(UpdateStaffCommand command);
     void handle(DeleteStaffCommand command);
+    Long handle(AddContractToStaffCommand command);
+    void handle(UpdateContractStatusCommand command);
 }
