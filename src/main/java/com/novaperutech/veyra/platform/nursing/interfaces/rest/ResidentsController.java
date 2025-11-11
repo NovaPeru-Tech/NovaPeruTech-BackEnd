e120bc1ee910a52f571fc78243eb4159687200f9
RenatoCY <renato.calvo2@hotmail.com>
2025-11-11 02:46:40 -0500

    fix: fixed resident controller

--- src/main/java/com/novaperutech/veyra/platform/nursing/interfaces/rest/ResidentsController.java
+++ src/main/java/com/novaperutech/veyra/platform/nursing/interfaces/rest/ResidentsController.java
@@ -1,7 +1,8 @@
 package com.novaperutech.veyra.platform.nursing.interfaces.rest;
 
 import com.novaperutech.veyra.platform.nursing.domain.model.commands.DeleteResidentCommand;
-import com.novaperutech.veyra.platform.nursing.domain.model.queries.GetAllResidentsQuery;
+import com.novaperutech.veyra.platform.nursing.domain.model.queries.GetActiveResidentsByNursingHomeId;
+import com.novaperutech.veyra.platform.nursing.domain.model.queries.GetAllResidentsByNursingHomeIdQuery;
 import com.novaperutech.veyra.platform.nursing.domain.model.queries.GetResidentByIdQuery;
 import com.novaperutech.veyra.platform.nursing.domain.services.ResidentCommandServices;
 import com.novaperutech.veyra.platform.nursing.domain.services.ResidentQueryServices;
@@ -25,8 +26,8 @@ import java.util.List;
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
 @RestController
-@RequestMapping(value = "/api/v1/nursing", produces = APPLICATION_JSON_VALUE)
-@Tag(name = "Residents",description = "Available ")
+@RequestMapping(value = "/api/v1/residents", produces = APPLICATION_JSON_VALUE)
+@Tag(name = "Residents",description = "Available resident endpoints")
 public class ResidentsController {
 private final ResidentCommandServices residentCommandServices;
 private final ResidentQueryServices residentQueryServices;
@@ -35,15 +36,16 @@ private final ResidentQueryServices residentQueryServices;
         this.residentCommandServices = residentCommandServices;
         this.residentQueryServices = residentQueryServices;
     }
-    @PostMapping
-    @Operation(summary = "Create a new Resident")
+    @PostMapping("/nursingHome/{nursingHomeId}")
+    @Operation(summary = "Create a new resident in a nursing home")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "201",description = "Resident created"),
             @ApiResponse(responseCode = "400",description = "Bad request")
     })
-    public ResponseEntity<ResidentResource>createResident(@RequestBody CreateResidentResource resource)
+    @Parameter(name = "nursingHomeId", description = "The unique identifier of the nursing home", required = true)
+    public ResponseEntity<ResidentResource>createResident(@RequestBody CreateResidentResource resource, @PathVariable Long nursingHomeId)
     {
-       var residentCommand= CreateResidentCommandFromResourceAssembler.toCommandFromResource(resource);
+       var residentCommand= CreateResidentCommandFromResourceAssembler.toCommandFromResource(resource,nursingHomeId);
        var residentId= residentCommandServices.handle(residentCommand);
        if (residentId==null||residentId==0L){return ResponseEntity.badRequest().build();}
        var getResidentByIdQuery=new GetResidentByIdQuery(residentId);
@@ -54,15 +56,15 @@ private final ResidentQueryServices residentQueryServices;
        return new ResponseEntity<>(residentResource, HttpStatus.CREATED);
     }
     @PutMapping("/{residentId}")
-    @Operation(summary = "Updated  for resident by id")
+    @Operation(summary = "Update resident by ID")
     @ApiResponses(value = {
-            @ApiResponse(responseCode = "200",description = "update resident"),
-            @ApiResponse(responseCode = "400",description = "resident not found ")
+            @ApiResponse(responseCode = "200", description = "Resident updated"),
+            @ApiResponse(responseCode = "404", description = "Resident not found")
     })
-    @Parameter(name = "residentId",description = " The unique identifier of the resident",required = true)
+    @Parameter(name = "residentId", description = "The unique identifier of the resident", required = true)
     public ResponseEntity<ResidentResource>updateResident(@PathVariable Long residentId, @RequestBody UpdateResidentResource resource)
     {
-      var updateResidentCommand= UpdateResidentCommandFromResourceAssembler.toCommandFromResource(residentId, resource);
+      var updateResidentCommand= UpdateResidentCommandFromResourceAssembler.toCommandFromResource(residentId,resource);
        var updateResident= residentCommandServices.handle(updateResidentCommand);
        if (updateResident.isEmpty()){return ResponseEntity.notFound().build();}
        var residentEntity=updateResident.get();
@@ -70,7 +72,7 @@ private final ResidentQueryServices residentQueryServices;
        return ResponseEntity.ok(residentResource);
     }
     @GetMapping("/{residentId}")
-    @Operation(summary = "Get resident by id")
+    @Operation(summary = "Get resident by ID")
     @ApiResponses(value={
             @ApiResponse(responseCode = "200",description = "resident found "),
             @ApiResponse(responseCode = "404",description = "resident not found")
@@ -85,28 +87,42 @@ private final ResidentQueryServices residentQueryServices;
     }
 
 
-    @GetMapping
-    @Operation(summary = "Get all residents")
+    @GetMapping("/nursingHome/{nursingHomeId}")
+    @Operation(summary = "Get all residents by nursing home")
     @ApiResponses(value = {
-            @ApiResponse(responseCode = "200",description = "residents found"),
-            @ApiResponse(responseCode = "404",description = "residents not found")
+            @ApiResponse(responseCode = "200",description = "Residents found"),
+            @ApiResponse(responseCode = "404",description = "Residents not found")
     })
-    public ResponseEntity<List<ResidentResource>>getAllResidents(){
-        var resident= residentQueryServices.handle(new GetAllResidentsQuery());
+    @Parameter(name = "nursingHomeId", description = "The unique identifier of the nursing home", required = true)
+    public ResponseEntity<List<ResidentResource>>getAllResidents(@PathVariable Long nursingHomeId){
+        var resident= residentQueryServices.handle(new GetAllResidentsByNursingHomeIdQuery(nursingHomeId));
         if (resident.isEmpty()){return ResponseEntity.notFound().build();}
         var residentResource= resident.stream().map(ResidentResourceFromEntityAssembler::toResourceFromEntity).toList();
         return ResponseEntity.ok(residentResource);
     }
     @DeleteMapping("/{residentId}")
-    @Operation(summary = "Delete resident by id")
+    @Operation(summary = "Delete resident by ID",description = "Delete resident")
     @ApiResponses(value={
-            @ApiResponse(responseCode = "200",description = "resident found "),
-            @ApiResponse(responseCode = "404",description = "resident not found")
+            @ApiResponse(responseCode = "200",description = "Resident deleted"),
+            @ApiResponse(responseCode = "404",description = "Resident not found")
     })
-    @Parameter(name = "residentId", description = " The unique identifier of the resident",required = true)
+    @Parameter(name = "residentId", description = "The unique identifier of the resident", required = true)
     public ResponseEntity<?>deleteResident( @PathVariable Long residentId){
      var deleteResidentCommand= new DeleteResidentCommand(residentId);
       residentCommandServices.handle(deleteResidentCommand);
-      return ResponseEntity.ok("Resident with give id successfully deleted");
+      return ResponseEntity.ok("Resident with given id successfully deleted");
+    }
+    @GetMapping("/nursingHome/{nursingHomeId}/active")
+    @Operation(summary = "Get all active residents by nursing home")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200",description = "residents actives found"),
+            @ApiResponse(responseCode = "404",description = "residents  actives not found")
+    })
+    @Parameter(name = "nursingHomeId", description = "The unique identifier of the nursing home", required = true)
+    public ResponseEntity<List<ResidentResource>>getAllResidentActives(@PathVariable Long nursingHomeId){
+        var resident= residentQueryServices.handle( new GetActiveResidentsByNursingHomeId(nursingHomeId));
+        if (resident.isEmpty()){return ResponseEntity.notFound().build();}
+        var residentResource=resident.stream().map(ResidentResourceFromEntityAssembler::toResourceFromEntity).toList();
+        return ResponseEntity.ok(residentResource);
     }
 }
