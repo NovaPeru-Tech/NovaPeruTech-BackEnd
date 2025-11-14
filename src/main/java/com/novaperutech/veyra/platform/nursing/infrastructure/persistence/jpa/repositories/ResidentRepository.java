60a5651d25d84f1c4b3e34776c08e44f7948e31b
RenatoCY <renato.calvo2@hotmail.com>
2025-11-14 04:06:08 -0500

    feat(nursing): implement news queries

--- src/main/java/com/novaperutech/veyra/platform/nursing/infrastructure/persistence/jpa/repositories/ResidentRepository.java
+++ src/main/java/com/novaperutech/veyra/platform/nursing/infrastructure/persistence/jpa/repositories/ResidentRepository.java
@@ -1,9 +1,12 @@
 package com.novaperutech.veyra.platform.nursing.infrastructure.persistence.jpa.repositories;
 
 import com.novaperutech.veyra.platform.nursing.domain.model.aggregates.Resident;
+import com.novaperutech.veyra.platform.nursing.domain.model.entities.MedicationAdministration;
 import com.novaperutech.veyra.platform.nursing.domain.model.valueobjects.PersonProfileId;
 import com.novaperutech.veyra.platform.nursing.domain.model.valueobjects.ResidentState;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -16,4 +19,5 @@ public interface ResidentRepository extends JpaRepository<Resident,Long> {
 Optional<Resident>findByNursingHomeIdAndId(Long nursingHome_id, Long id);
     List<Resident> findByNursingHomeIdAndResidentStatus(Long nursingHomeId, ResidentState residentState);
     boolean existsByNursingHomeIdAndPersonProfileId(Long nursingHomeId, PersonProfileId personProfileId);
-}
+    @Query("SELECT ma FROM Resident r JOIN r.medicationAdministrationHistory.medicationAdministrations ma WHERE r.id = :residentId")
+    List<MedicationAdministration> findMedicationAdministrationsByResidentId(@Param("residentId") Long residentId);}
