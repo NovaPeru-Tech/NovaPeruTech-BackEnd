1c2ad216cff0f55edc405ec1085f17651b665857
RenatoCY <renato.calvo2@hotmail.com>
2025-11-14 04:07:02 -0500

    feat(nursing): implement news queries and commands

--- src/main/java/com/novaperutech/veyra/platform/nursing/domain/services/ResidentCommandServices.java
+++ src/main/java/com/novaperutech/veyra/platform/nursing/domain/services/ResidentCommandServices.java
@@ -1,6 +1,7 @@
 package com.novaperutech.veyra.platform.nursing.domain.services;
 
 import com.novaperutech.veyra.platform.nursing.domain.model.aggregates.Resident;
+import com.novaperutech.veyra.platform.nursing.domain.model.commands.AddMedicationAdministrationCommand;
 import com.novaperutech.veyra.platform.nursing.domain.model.commands.CreateResidentCommand;
 import com.novaperutech.veyra.platform.nursing.domain.model.commands.DeleteResidentCommand;
 import com.novaperutech.veyra.platform.nursing.domain.model.commands.UpdateResidentCommand;
@@ -11,4 +12,5 @@ public interface ResidentCommandServices {
     Long handle (CreateResidentCommand command);
     Optional<Resident>handle(UpdateResidentCommand command);
     void handle (DeleteResidentCommand command);
+    void handle (AddMedicationAdministrationCommand command);
 }
--- src/main/java/com/novaperutech/veyra/platform/nursing/domain/services/ResidentQueryServices.java
+++ src/main/java/com/novaperutech/veyra/platform/nursing/domain/services/ResidentQueryServices.java
@@ -1,6 +1,7 @@
 package com.novaperutech.veyra.platform.nursing.domain.services;
 
 import com.novaperutech.veyra.platform.nursing.domain.model.aggregates.Resident;
+import com.novaperutech.veyra.platform.nursing.domain.model.entities.MedicationAdministration;
 import com.novaperutech.veyra.platform.nursing.domain.model.queries.*;
 
 import java.util.List;
@@ -11,4 +12,5 @@ public interface ResidentQueryServices {
     Optional<Resident>handle(GetResidentByIdQuery query);
     Optional<Resident>handle(GetResidentByPersonProfileQuery query);
     List<Resident>handle(GetActiveResidentsByNursingHomeId query);
+    List<MedicationAdministration>handle(GetMedicationAdministrationsByResidentIdQuery query);
 }
