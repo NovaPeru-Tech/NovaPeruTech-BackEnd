9f15912be5031ba7f663e3086ce8495fb5d21cdb
RenatoCY <renato.calvo2@hotmail.com>
2025-11-16 03:29:51 -0500

    refactor(nursing): refactorization for repositories

--- src/main/java/com/novaperutech/veyra/platform/nursing/infrastructure/persistence/jpa/repositories/MedicationRepository.java
+++ src/main/java/com/novaperutech/veyra/platform/nursing/infrastructure/persistence/jpa/repositories/MedicationRepository.java
@@ -5,12 +5,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
-import java.util.Optional;
 
 @Repository
 public interface MedicationRepository extends JpaRepository<Medication,Long> {
     boolean existsByResidentIdAndName(Long residentId, String name);
 
-    Optional<Medication>findByName(String name);
+    boolean existsByResidentId(Long residentId);
     List<Medication> findByResidentId(Long residentId);
 }
--- src/main/java/com/novaperutech/veyra/platform/nursing/infrastructure/persistence/jpa/repositories/ResidentRepository.java
+++ src/main/java/com/novaperutech/veyra/platform/nursing/infrastructure/persistence/jpa/repositories/ResidentRepository.java
@@ -1,12 +1,9 @@
 package com.novaperutech.veyra.platform.nursing.infrastructure.persistence.jpa.repositories;
 
 import com.novaperutech.veyra.platform.nursing.domain.model.aggregates.Resident;
-import com.novaperutech.veyra.platform.nursing.domain.model.entities.MedicationAdministration;
 import com.novaperutech.veyra.platform.nursing.domain.model.valueobjects.PersonProfileId;
 import com.novaperutech.veyra.platform.nursing.domain.model.valueobjects.ResidentState;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -15,9 +12,8 @@ import java.util.Optional;
 @Repository
 public interface ResidentRepository extends JpaRepository<Resident,Long> {
     Optional<Resident>findByPersonProfileId( PersonProfileId personProfileId);
-    List<Resident>findByNursingHomeId(Long nursingHomeId);
-Optional<Resident>findByNursingHomeIdAndId(Long nursingHome_id, Long id);
+    List<Resident>findAllByNursingHomeId(Long nursingHomeId);
     List<Resident> findByNursingHomeIdAndResidentStatus(Long nursingHomeId, ResidentState residentState);
     boolean existsByNursingHomeIdAndPersonProfileId(Long nursingHomeId, PersonProfileId personProfileId);
-    @Query("SELECT ma FROM Resident r JOIN r.medicationAdministrationHistory.medicationAdministrations ma WHERE r.id = :residentId")
-    List<MedicationAdministration> findMedicationAdministrationsByResidentId(@Param("residentId") Long residentId);}
+}
+
