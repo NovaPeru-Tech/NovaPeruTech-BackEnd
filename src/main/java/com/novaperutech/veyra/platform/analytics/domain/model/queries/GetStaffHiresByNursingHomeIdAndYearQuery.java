25dab89d5baf1a0a811d38244d2ee84f9fdfb742
RenatoCY <renato.calvo2@hotmail.com>
2025-11-19 19:48:06 -0500

    feat(analytics): added queries for staff metrics

---
+++ src/main/java/com/novaperutech/veyra/platform/analytics/domain/model/queries/GetStaffHiresByNursingHomeIdAndYearQuery.java
@@ -0,0 +1,15 @@
+package com.novaperutech.veyra.platform.analytics.domain.model.queries;
+
+import com.novaperutech.veyra.platform.hcm.domain.model.valueobjetcs.NursingHomeId;
+
+public record GetStaffHiresByNursingHomeIdAndYearQuery(NursingHomeId nursingHomeId,Integer year) {
+    public  GetStaffHiresByNursingHomeIdAndYearQuery{
+        if (year==null){
+            throw new IllegalArgumentException("year cannot be null");
+        }
+        if ( year < 1900 || year > 2025) {
+            throw new IllegalArgumentException("year must be between 1900 and 2025");
+        }
+    }
+
+}
---
+++ src/main/java/com/novaperutech/veyra/platform/analytics/domain/model/queries/GetStaffTerminationsByNursingHomeIdAndYearAndMonthQuery.java
@@ -0,0 +1,22 @@
+package com.novaperutech.veyra.platform.analytics.domain.model.queries;
+
+import com.novaperutech.veyra.platform.analytics.domain.model.valueobjects.NursingHomeId;
+
+public record GetStaffTerminationsByNursingHomeIdAndYearAndMonthQuery(NursingHomeId nursingHomeId, Integer year,Integer moth) {
+public   GetStaffTerminationsByNursingHomeIdAndYearAndMonthQuery{
+    if (year==null){
+        throw new IllegalArgumentException("year must not be null");
+    }
+    if (year<1900||year>2025)
+    {
+        throw new IllegalArgumentException("year must be between 1900 and 2025");
+    }
+    if (moth==null){
+        throw new IllegalArgumentException("moth must not be null");
+    }
+    if (moth<1||moth>12)
+    {
+        throw new IllegalArgumentException("moth must be between 1 and 12");
+    }
+}
+}
