9de85da5ad1185b530f223ce1a1bb1252c455090
Adrian QC <aqc1019@gmail.com>
2025-11-27 16:34:28 -0500

    fix: correct uppercase fields.

--- src/main/java/com/novaperutech/veyra/platform/hcm/interfaces/rest/resources/StaffResource.java
+++ src/main/java/com/novaperutech/veyra/platform/hcm/interfaces/rest/resources/StaffResource.java
@@ -1,6 +1,6 @@
 package com.novaperutech.veyra.platform.hcm.interfaces.rest.resources;
 
 
-public record StaffResource(Long id, Long profileId,String status,
+public record StaffResource(Long id, Long personProfileId, String status,
          String emergencyContactFirstName, String emergencyContactLastName, String emergencyContactPhoneNumber) {
 }
