49ed600e06825dc48d851a82fba334efcf5fa170
RenatoCY <renato.calvo2@hotmail.com>
2025-11-30 23:37:16 -0500

    feat(health): add value objects

---
+++ src/main/java/com/novaperutech/veyra/platform/health/domain/model/valueobjects/MeasurementId.java
@@ -0,0 +1,8 @@
+package com.novaperutech.veyra.platform.health.domain.model.valueobjects;
+
+import jakarta.persistence.Embeddable;
+
+@Embeddable
+public record MeasurementId(Long measurementId) {
+
+}
