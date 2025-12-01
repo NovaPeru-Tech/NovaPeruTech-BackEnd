ab92706a4271eceb025cc53b12bc9ccea514a3c4
RenatoCY <renato.calvo2@hotmail.com>
2025-11-30 23:38:12 -0500

    feat(health): add layer application internal

---
+++ src/main/java/com/novaperutech/veyra/platform/health/application/internal/eventhandlers/AbnormalVitalSignDetectedEventHandler.java
@@ -0,0 +1,7 @@
+package com.novaperutech.veyra.platform.health.application.internal.eventhandlers;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class AbnormalVitalSignDetectedEventHandler {
+}
---
+++ src/main/java/com/novaperutech/veyra/platform/health/application/internal/queryservices/VitalSignQueryServiceImpl.java
@@ -0,0 +1,8 @@
+package com.novaperutech.veyra.platform.health.application.internal.queryservices;
+
+import com.novaperutech.veyra.platform.health.domain.services.VitalSignQueryService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class VitalSignQueryServiceImpl implements VitalSignQueryService {
+}
