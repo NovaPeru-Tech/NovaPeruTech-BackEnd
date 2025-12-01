317091de954a93f9b712058c16906ce36196c687
RenatoCY <renato.calvo2@hotmail.com>
2025-11-30 23:29:32 -0500

    chore: remove outdated bounded context family

--- src/main/java/com/novaperutech/veyra/platform/family/domain/model/queries/GetResidentsByUserIdQuery.java
+++
@@ -1,6 +0,0 @@
-package com.novaperutech.veyra.platform.family.domain.model.queries;
-
-import com.novaperutech.veyra.platform.family.domain.model.valueobjects.UserId;
-
-public record GetResidentsByUserIdQuery(UserId userId) {
-}
--- src/main/java/com/novaperutech/veyra/platform/family/domain/services/AccessCodeQueryService.java
+++
@@ -1,4 +0,0 @@
-package com.novaperutech.veyra.platform.family.domain.services;
-
-public interface AccessCodeQueryService {
-}
