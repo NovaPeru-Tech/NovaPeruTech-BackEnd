3c66be57110445a9ceb18b06c547b25d54df41f2
RenatoCY <renato.calvo2@hotmail.com>
2025-12-02 19:41:33 -0500

    fix: correct spelling of 'aggregates' in Allergy and VitalSign classes

--- src/main/java/com/novaperutech/veyra/platform/health/domain/services/AllergyQueryService.java
+++ src/main/java/com/novaperutech/veyra/platform/health/domain/services/AllergyQueryService.java
@@ -1,6 +1,6 @@
 package com.novaperutech.veyra.platform.health.domain.services;
 
-import com.novaperutech.veyra.platform.health.domain.model.aggreagtes.Allergy;
+import com.novaperutech.veyra.platform.health.domain.model.aggregates.Allergy;
 import com.novaperutech.veyra.platform.health.domain.model.queries.ExistsAllergyByResidentIdQuery;
 import com.novaperutech.veyra.platform.health.domain.model.queries.GetAllergiesByResidentIdQuery;
 import com.novaperutech.veyra.platform.health.domain.model.queries.GetAllergyByIdQuery;
