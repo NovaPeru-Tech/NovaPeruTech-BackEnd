9554f0042776d40289acc13603136187a43e1b75
RenatoCY <renato.calvo2@hotmail.com>
2025-12-04 17:35:06 -0500

    feat(tracking): add resources and assemblers

---
+++ src/main/java/com/novaperutech/veyra/platform/tracking/interfaces/rest/resources/AssignDeviceResource.java
@@ -0,0 +1,5 @@
+package com.novaperutech.veyra.platform.tracking.interfaces.rest.resources;
+
+public record AssignDeviceResource( Long residentId,
+                                    String assignedBy) {
+}
