72dc4f025552f49f25fd0c27c0a957c76d99167c
RenatoCY <renato.calvo2@hotmail.com>
2025-12-05 19:04:49 -0500

    feat(payments): add commands for payments

--- src/main/java/com/novaperutech/veyra/platform/payments/domain/model/commands/UpdateSubscriptionCommand.java
+++ src/main/java/com/novaperutech/veyra/platform/payments/domain/model/commands/UpdateSubscriptionCommand.java
@@ -1,4 +1,12 @@
 package com.novaperutech.veyra.platform.payments.domain.model.commands;
-
-public record UpdateSubscriptionCommand() {
+public record UpdateSubscriptionCommand(
+        Long subscriptionId,
+        String planType,
+        String period
+) {
+    public UpdateSubscriptionCommand {
+        if (subscriptionId == null) {
+            throw new IllegalArgumentException("Subscription ID cannot be null");
+        }
+    }
 }
