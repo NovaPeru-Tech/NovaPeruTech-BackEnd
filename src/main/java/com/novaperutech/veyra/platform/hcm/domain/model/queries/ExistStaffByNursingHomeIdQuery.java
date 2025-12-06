25976faa99e13f1aaef14f28f0be0b2375e2e936
RenatoCY <renato.calvo2@hotmail.com>
2025-12-06 00:49:31 -0500

    feat: implement news api response

---
+++ src/main/java/com/novaperutech/veyra/platform/hcm/domain/model/queries/ExistStaffByNursingHomeIdQuery.java
@@ -0,0 +1,7 @@
+package com.novaperutech.veyra.platform.hcm.domain.model.queries;
+
+import com.novaperutech.veyra.platform.hcm.domain.model.valueobjects.NursingHomeId;
+
+public record ExistStaffByNursingHomeIdQuery(NursingHomeId nursingHomeId) {
+
+}
